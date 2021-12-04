b503dd0dcf150798756259a27ccb46b522cb03e1
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-04 18:31:17 +0100

    display ingredients on recipe details page

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,7 +1,10 @@
+import { Ingredient } from "../models/ingredient";
+
 export class Recipe {
   constructor(
     public name: string,
     public description: string,
-    public imagePath: string
+    public imagePath: string,
+    public ingredients: Ingredient[]
   ) {}
 }
--- src/app/services/recipe.ts
+++ src/app/services/recipe.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../models/ingredient";
 import { Recipe } from "../recipes/recipe.model";
 
 @Injectable({ providedIn: "root" })
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       "Bang Bang Shrimp Pasta",
       "This Bang Bang Shrimp Pasta has the most scrumptious, creamy sauce.",
-      "https://www.theidearoom.net/wp-content/uploads/2017/05/bang-bang-shrimp-pasta-recipe-theidearoom-1-1.jpg"
+      "https://www.theidearoom.net/wp-content/uploads/2017/05/bang-bang-shrimp-pasta-recipe-theidearoom-1-1.jpg",
+      [new Ingredient("Shrimp", 2), new Ingredient("Pasta", 1)]
     ),
     new Recipe(
       "Dum Aloo",
       "Creamy, rich and spicy Dum Aloo recipe, golden fried baby potatoes cooked in a delicious gravy!",
-      "https://i0.wp.com/cookingfromheart.com/wp-content/uploads/2017/08/Dum-Aloo-5.jpg?w=1024&ssl=1"
+      "https://i0.wp.com/cookingfromheart.com/wp-content/uploads/2017/08/Dum-Aloo-5.jpg?w=1024&ssl=1",
+      [new Ingredient("Potato", 2), new Ingredient("Yogurt", 1)]
     ),
   ];
   constructor() {}
