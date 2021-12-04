617ca9d67af5b180596774fdf3500f4143cc674a
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-04 14:12:49 +0100

    add dropdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { FormsModule } from '@angular/forms';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { FormsModule } from "@angular/forms";
+import { DropdownDirective } from "./directives/dropdown";
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { FormsModule } from '@angular/forms';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
---
+++ src/app/directives/dropdown.ts
@@ -0,0 +1,9 @@
+import { Directive, HostBinding, HostListener } from "@angular/core";
+
+@Directive({ selector: "[appDropdown]" })
+export class DropdownDirective {
+  @HostBinding("class.open") isOpen = false;
+  @HostListener("click") toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}
