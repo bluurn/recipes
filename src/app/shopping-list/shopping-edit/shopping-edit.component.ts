17dd51fcb1e851f715ec9f07e71990db964324ea
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-10 11:34:34 +0100

    reset the form after submit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -46,7 +46,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const ingredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -57,5 +57,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.shoppingListService.add(ingredient);
     }
+    this.editMode = false;
+    form.reset();
   }
 }
