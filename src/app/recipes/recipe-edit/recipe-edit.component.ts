8b425cf224c7fca064f5e8abc94f585d7eeaaddb
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-10 12:31:40 +0100

    init form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 import { ActivatedRoute, Params } from "@angular/router";
+import { RecipeService } from "src/app/services/recipe.service";
 
 @Component({
   selector: "app-recipes",
@@ -9,12 +11,35 @@ export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
 
-  constructor(private route: ActivatedRoute) {}
+  recipeForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
       this.editMode = params["id"] != null;
+      this.initForm();
+    });
+  }
+
+  initForm() {
+    let recipeName = "";
+    let imagePath = "";
+    let description = "";
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      imagePath = recipe.imagePath;
+      description = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(imagePath),
+      description: new FormControl(description),
     });
   }
 }
