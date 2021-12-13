e1cb8799c2445470c79bbc9846a3e77f732878e8
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-13 14:18:20 +0100

    handling form input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   isLoginMode = true;
 
+  authForm: FormGroup;
+
+  ngOnInit(): void {
+    this.initForm();
+  }
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    console.log(this.authForm.value);
+    this.authForm.reset();
+  }
+  private initForm() {
+    this.authForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 }
