b042f6fabbad1dc4f51e78b79c2ad280a1358e7e
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-13 13:47:56 +0100

    check if recipes are fetched

--- src/app/services/recipes-resolver.service.ts
+++ src/app/services/recipes-resolver.service.ts
@@ -7,14 +7,23 @@ import {
 import { Observable } from "rxjs";
 import { Recipe } from "../recipes/recipe.model";
 import { DataStorageService } from "./data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({ providedIn: "root" })
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Recipe[] | Observable<Recipe[]> | Promise<Recipe[]> {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0) {
+      return this.dataStorageService.fetchRecipes();
+    }
+
+    return recipes;
   }
 }
