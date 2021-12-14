e866e0047892c49d80b6db8223a472b5021789ef
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-14 16:01:45 +0100

    lazy loading recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing.module";
 import { HeaderComponent } from "./header/header.component";
 import { HttpClientModule } from "@angular/common/http";
-import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
@@ -20,7 +19,6 @@ import { AuthModule } from "./auth/auth.module";
     SharedModule,
     AppRoutingModule,
     AuthModule,
-    RecipesModule,
     ShoppingListModule,
   ],
   bootstrap: [AppComponent],
