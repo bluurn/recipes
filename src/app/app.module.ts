110592061b2b6ffd2808d103aba546f15b2f72c1
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-15 16:40:27 +0100

    add store devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,15 @@ import { StoreModule } from "@ngrx/store";
 import * as fromApp from "./store/app.reducer";
 import { EffectsModule } from "@ngrx/effects";
 import { AuthEffects } from "./auth/store/auth.effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { environment } from "src/environments/environment";
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
     EffectsModule.forRoot([AuthEffects]),
     CoreModule,
     SharedModule,
