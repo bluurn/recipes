9c3b0f897f3fb9c8b9cf0afc3adace60b69b44f7
vladimir.suvorov <vladimir.suvorov@xempus.com>
2021-12-15 13:13:59 +0100

    auth reducer and actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from "../user.model";
+import * as AuthActions from "./auth.actions";
 
 export interface State {
   user: User;
@@ -8,6 +9,27 @@ const initialState: State = {
   user: null,
 };
 
-export function authReducer(state: State = initialState, action): State {
-  return state;
+export function authReducer(
+  state: State = initialState,
+  action: AuthActions.AuthActions
+): State {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      return {
+        ...state,
+        user: new User(
+          action.payload.email,
+          action.payload.userId,
+          action.payload.token,
+          action.payload.expirationDate
+        ),
+      };
+    case AuthActions.LOGOUT:
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
 }
